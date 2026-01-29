d097b5ea163825b2a2c617697503407cba0d8ce8
Xiangyu Zhang <35476757+xiangyu2022@users.noreply.github.com>
2026-01-28 18:34:04 -0600

    Add files via upload

--- Code_Sec5_Proj_Boot.R
+++ Code_Sec5_Proj_Boot.R
@@ -155,77 +155,68 @@ norm_score_f2 = score_f$norm_score_func2
 norm_score_f3 = score_f$norm_score_func3
 
 #########################   K2   ######################### 
-
-
-### Specify isometry and apply it to basis h and orthonormalzied score functions b
+### Specify isometry and apply it to the orthornomal basis functions h and orthonormalzied score functions b
 M1=6
-isometry1=function(x) sqrt(densf(x,MLEf)/densg1(x,MLEg1))
-l1h=list()
+isometry=function(x) sqrt(densf(x,MLEf)/densg1(x,MLEg1))
+lh=list();
 for (i in 1:(M1)){
-  for (j in 1){
-    call = paste("l",j,"h[[",i,"]]<-function(x) isometry",j,"(x) * hj[[",i+1,"]](G_f(x,MLEf))",sep="")
+    call = paste("lh[[",i,"]]<-function(x) isometry(x) * hj[[",i+1,"]](G_f(x,MLEf))",sep="")
     eval(parse(text=call))
-  }
 }
-l1b=list()
+lb=list()
 for (i in 1:3){
-  for (j in 1){
-    call = paste("l",j,"b[[",i,"]]<-function(x) isometry",j,"(x) *norm_score_f",i,"(x)",sep="")
+    call = paste("lb[[",i,"]]<-function(x) isometry(x) *norm_score_f",i,"(x)",sep="")
     eval(parse(text=call))
-  }
+
 }
 
 ### Calculate necessary inner products for K
-innerl1 = integrate(function(x) {sqrt(densf(x,MLEf)*densg1(x,MLEg1))},lower=L1,upper=U1,rel.tol=1e-8)$value
-inner1lh = inner1lb = c()
+innerl = integrate(function(x) {sqrt(densf(x,MLEf)*densg1(x,MLEg1))},lower=L1,upper=U1,rel.tol=1e-8)$value
+innerlh = innerlb = c()
 for (i in 1:(M1)){
-  inner1lh[i]=integrate(function(x) {l1h[[i]](x)*densg1(x,MLEg1)},lower=L1,upper=U1,rel.tol=1e-8)$value
+  innerlh[i]=integrate(function(x) {lh[[i]](x)*densg1(x,MLEg1)},lower=L1,upper=U1,rel.tol=1e-8)$value
 }
 for (i in 1:3){
-  inner1lb[i]=integrate(function(x) {l1b[[i]](x)*densg1(x,MLEg1)},lower=L1,upper=U1,rel.tol=1e-8)$value
+  innerlb[i]=integrate(function(x) {lb[[i]](x)*densg1(x,MLEg1)},lower=L1,upper=U1,rel.tol=1e-8)$value
 }
 
 ### Apply K to lh and lb 
-Kl1h=list();Kl1b=list()
+Klh=list();Klb=list()
 for (i in 1:(M1)){
-  for (j in 1){
-    call = paste("Kl",j,"h[[",i,"]]<-function(x) l",j,"h[[",i,"]](x)- (1-isometry",j,"(x))/(1-innerl",j,")*inner",j,"lh[",i,"]",sep="")
+    call = paste("Klh[[",i,"]]<-function(x) lh[[",i,"]](x)- (1-isometry(x))/(1-innerl)*innerlh[",i,"]",sep="")
     eval(parse(text=call))
-  }
 }
 for (i in 1:3){
-  for (j in 1){
-    call = paste("Kl",j,"b[[",i,"]]<-function(x) l",j,"b[[",i,"]](x)- (1-isometry",j,"(x))/(1-innerl",j,")*inner",j,"lb[",i,"]",sep="")
+    call = paste("Klb[[",i,"]]<-function(x) lb[[",i,"]](x)- (1-isometry(x))/(1-innerl)*innerlb[",i,"]",sep="")
     eval(parse(text=call))
-  }
 }
 
 ### Calculate necessary inner products for U
-inner_step3_1_1 = integrate(function(x) {(norm_score_g11(x)-Kl1b[[1]](x))*Kl1b[[2]](x)*densg1(x,MLEg1)},lower=L1, upper=U1,rel.tol=1e-8)$value
-inner_step3_2_1 = integrate(function(x) {norm_score_g11(x)*Kl1b[[1]](x)*densg1(x,MLEg1)},lower=L1 ,upper=U1,rel.tol=1e-8)$value
-tildec2_g1 = function(x){Kl1b[[2]](x)-inner_step3_1_1/(1-inner_step3_2_1)*(norm_score_g11(x)-Kl1b[[1]](x))} # Same with below
+inner_step3_1_1 = integrate(function(x) {(norm_score_g11(x)-Klb[[1]](x))*Klb[[2]](x)*densg1(x,MLEg1)},lower=L1, upper=U1,rel.tol=1e-8)$value
+inner_step3_2_1 = integrate(function(x) {norm_score_g11(x)*Klb[[1]](x)*densg1(x,MLEg1)},lower=L1 ,upper=U1,rel.tol=1e-8)$value
+tildec2_g1 = function(x){Klb[[2]](x)-inner_step3_1_1/(1-inner_step3_2_1)*(norm_score_g11(x)-Klb[[1]](x))} # Same with below
 
-inner_step3_c3 = integrate(function(x) {(norm_score_g11(x)-Kl1b[[1]](x))*Kl1b[[3]](x)*densg1(x,MLEg1)},lower=L1, upper=U1,rel.tol=1e-8)$value
-Ua1c1c3 = function(x) {Kl1b[[3]](x) - inner_step3_c3/(1-inner_step3_2_1)*(norm_score_g11(x)-Kl1b[[1]](x))}
+inner_step3_c3 = integrate(function(x) {(norm_score_g11(x)-Klb[[1]](x))*Klb[[3]](x)*densg1(x,MLEg1)},lower=L1, upper=U1,rel.tol=1e-8)$value
+Ua1c1c3 = function(x) {Klb[[3]](x) - inner_step3_c3/(1-inner_step3_2_1)*(norm_score_g11(x)-Klb[[1]](x))}
 inner_a2ctile2minus = integrate(function(x) {(norm_score_g12(x)-tildec2_g1(x))*Ua1c1c3(x)*densg1(x,MLEg1)},lower=L1,upper=U1,rel.tol=1e-8)$value
 inner_a2ctile2 = integrate(function(x) {norm_score_g12(x)*tildec2_g1(x)*densg1(x,MLEg1)}, lower=L1 ,upper=U1,rel.tol=1e-8)$value
 tildec3_g1 = function(x) {Ua1c1c3(x) - inner_a2ctile2minus/(1-inner_a2ctile2)*(norm_score_g12(x)-tildec2_g1(x))}
 
 ### Apply U to Klh and Klb 
 inner1_h = inner_U1_h = inner_UU1_h=c()
-U1_h = UU1_h = UKl1h = list()
+U1_h = UU1_h = UKlh = list()
 inner_Ua2c2 = integrate(function(x) {norm_score_g12(x)*tildec2_g1(x)*densg1(x,MLEg1)},lower=L1,upper=U1,rel.tol=1e-8)$value
 inner_Ua3c3 = integrate(function(x) {norm_score_g13(x)*tildec3_g1(x)*densg1(x,MLEg1)},lower=L1,upper=U1,rel.tol=1e-8)$value
 
 for (i in 1:(M1)){
-  inner1_h[i]=integrate(function(x) {(norm_score_g11(x)-Kl1b[[1]](x))*Kl1h[[i]](x)*densg1(x,MLEg1)}, lower=L1, upper=U1, rel.tol=1e-6)$value
-  call1 = paste("U1_h[[",i,"]]<-function(x) {Kl1h[[",i,"]](x)-inner1_h[",i,"]/(1-inner_step3_2_1)*(norm_score_g11(x)-Kl1b[[1]](x))}",sep="")
+  inner1_h[i]=integrate(function(x) {(norm_score_g11(x)-Klb[[1]](x))*Klh[[i]](x)*densg1(x,MLEg1)}, lower=L1, upper=U1, rel.tol=1e-6)$value
+  call1 = paste("U1_h[[",i,"]]<-function(x) {Klh[[",i,"]](x)-inner1_h[",i,"]/(1-inner_step3_2_1)*(norm_score_g11(x)-Klb[[1]](x))}",sep="")
   eval(parse(text=call1))
   inner_U1_h[i] = integrate(function(x) {(norm_score_g12(x)-tildec2_g1(x))*U1_h[[i]](x)*densg1(x,MLEg1)}, lower=L1, upper=U1, rel.tol=1e-6)$value
   call2 = paste("UU1_h[[",i,"]]<-function(x) {U1_h[[",i,"]](x)-inner_U1_h[",i,"]/(1-inner_Ua2c2)*(norm_score_g12(x)-tildec2_g1(x))}",sep="")
   eval(parse(text=call2))
   inner_UU1_h[i] = integrate(function(x){(norm_score_g13(x)-tildec3_g1(x))*UU1_h[[i]](x)*densg1(x,MLEg1)},lower=L1,upper=U1,rel.tol=1e-8)$value
-  call3 = paste("UKl1h[[",i,"]]<-function(x) UU1_h[[",i,"]](x)- inner_UU1_h[",i,"]/(1-inner_Ua3c3)*(norm_score_g13(x) - tildec3_g1(x))",sep="")
+  call3 = paste("UKlh[[",i,"]]<-function(x) UU1_h[[",i,"]](x)- inner_UU1_h[",i,"]/(1-inner_Ua3c3)*(norm_score_g13(x) - tildec3_g1(x))",sep="")
   eval(parse(text=call3))
 }
 
@@ -258,7 +249,7 @@ for (i in 1:M1){
                   ,"]](G_f(x,MLEf))*norm_score_f2(x)*densf(x,MLEf) }, L1, U1, rel.tol = 1e-8)$value",sep="")
   call003 = paste("inner",i,"_13=integrate(function(x) {hj[[",i+1
                   ,"]](G_f(x,MLEf))*norm_score_f3(x)*densf(x,MLEf) }, L1, U1, rel.tol = 1e-8)$value",sep="")
-  call004 = paste("Kvj1[[",i+1,"]] = function(x) {UKl1h[[",i,"]](x) - norm_score_g11(x) * inner",i,
+  call004 = paste("Kvj1[[",i+1,"]] = function(x) {UKlh[[",i,"]](x) - norm_score_g11(x) * inner",i,
                   "_11 - norm_score_g12(x) * inner",i,"_12 - norm_score_g13(x) * inner",i,"_13 }",sep="")
   eval(parse(text=call001))
   eval(parse(text=call002))
@@ -278,115 +269,20 @@ G_g1<-function(x,pars){
 set.seed(1)
 start_time1 <- Sys.time()
 B <- 100000
-res_tn_order_boot_q <- res_tn_boot_q <-res_gst_boot_q <- res_ks_boot_q <- numeric(B)
-vj_boot_q <- matrix(NA,nrow=B,ncol=M1)
+res_tn_order_boot_f <- res_tn_boot_f  <- numeric(B); vj_boot_q <- matrix(NA,nrow=B,ncol=M1)
 for (k in 1:B){
   num = rmultinom(n=1,size=n,prob=c(1-sum(MLEf),MLEf))
-  xx_q = c(runif(num[1],L1,U1),rtruncnorm(num[2], a=L1, b=U1, mean = pos[1], sd = 0.05),
+  xx_f = c(runif(num[1],L1,U1),rtruncnorm(num[2], a=L1, b=U1, mean = pos[1], sd = 0.05),
            rtruncnorm(num[3], a=L1, b=U1, mean = pos[2], sd = 0.05), 
            rtruncnorm(num[4], a=L1, b=U1, mean = pos[3], sd = 0.05))
   for (j in 1:M1){
-    vj_boot_q[k,j] <- sqrt(n)*mean(vj[[j+1]](xx_q))}
-  res_tn_boot_q[k] <- max(cumsum((vj_boot_q[k,])^2)/1:M1)
-  res_tn_order_boot_q[k] <- max(cumsum(sort((vj_boot_q[k,])^2,decreasing=TRUE))/1:M1)
+    vj_boot_q[k,j] <- sqrt(n)*mean(vj[[j+1]](xx_f))}
+  res_tn_boot_f[k] <- max(cumsum((vj_boot_q[k,])^2)/1:M1)
+  res_tn_order_boot_f[k] <- max(cumsum(sort((vj_boot_q[k,])^2,decreasing=TRUE))/1:M1)
   print(k)
 }
 end_time1 <- Sys.time()
 end_time1-start_time1
-
-
-###### From g1 ######
-sampler_c = function(n, cc){
-  tt = rlst(2*n,df=4,sigma=0.025)
-  res = (tt[tt>=L1+0.015-cc & tt<=U1-0.015-cc])[1:n]
-  return(rtruncnorm(n,a=cc-0.015,b=cc+0.015,cc,sigma) + res)
-}
-sampler_x1 = function(n){
-  indx = rmultinom(1,size=n,prob=c(1-sum(MLEg1),MLEg1))
-  return (c(runif(indx[1],L1,U1), sampler_c(indx[2],pos[1]), sampler_c(indx[3],pos[2]),sampler_c(indx[4],pos[3])))
-}
-gradg1_func <- function(xx,pars){
-  grad1 <- function(x) (-bkg1(x) + fw_norm(x,pos[1]))/densg1(x,pars)
-  grad2 <- function(x) (-bkg1(x) + fw_norm(x,pos[2]))/densg1(x,pars)
-  grad3 <- function(x) (-bkg1(x) + fw_norm(x,pos[3]))/densg1(x,pars)
-  return(c(-sum(grad1(xx)), -sum(grad2(xx)), -sum(grad3(xx))))
-}
-set.seed(1)
-start_time2 <- Sys.time()
-res_ks_boot1 <- res_gst_boot1<- res_tn_boot1 <- res_tn_order_boot1 <- numeric(B)
-S1_boot <- matrix(NA,n,M1)
-vj_boot<- matrix(NA,nrow=B,ncol=M1)
-for (k in 1:B){
-  xx_boot = sampler_x1(n)
-  for (j in 1:M1){
-    vj_boot[k,j] <- sqrt(n)*mean(hj[[j+1]](G_g1(xx_boot,MLEg1)))
-  }
-  res_tn_boot1[k] <- max(cumsum((vj_boot[k,])^2)/1:M1)
-  res_tn_order_boot1[k] <- max(cumsum(sort((vj_boot[k,])^2,decreasing=TRUE))/1:M1)
-  print(k)
-}
-end_time2 <- Sys.time()
-end_time2-start_time2
-
-###### From K2g1 ######
-set.seed(1)
-start_time3 <- Sys.time()
-res_tn_order_boot_K2_g1 <- res_tn_boot_K2_g1 <-res_gst_boot_K2_g1<-res_ks_boot_K2_g1 <- numeric(B)
-vn_boot_K2_g1 <- matrix(NA,nrow=B,ncol=M1)
-for (k in 1:B){
-  xx_k2 = sampler_x1(n)
-  for (j in 1:M1){
-    vn_boot_K2_g1[k,j] <- sqrt(n)*mean(Kvj1[[j+1]](xx_k2))
-  }
-  res_ks_boot_K2_g1[k] <- max(abs(vn_boot_K2_g1[k,]))
-  res_tn_boot_K2_g1[k] <- max(cumsum((vn_boot_K2_g1[k,])^2)/1:M1)
-  res_tn_order_boot_K2_g1[k] <- max(cumsum(sort((vn_boot_K2_g1[k,])^2,decreasing=TRUE))/1:M1)
-  print(k)
-}
-end_time3 <- Sys.time()
-end_time3-start_time3
-
-
-#################################### QQplots #################################### 
-setEPS()
-postscript("MNRAS1.eps",width = 9, height = 6)
-par(mar = c(4, 5.5, 2, 2))
-qqplot(res_tn_boot_g1,res_tn_boot_q, ylab=expression("Quantiles of "*hat(T)[n]*" from "*G[beta]),
-       xlab=expression("Quantiles of "*hat(T)[n]*" from "*F[gamma]*""),
-       lwd = 2, cex.axis = 1.6, cex.lab = 1.8)
-abline(0,1)
-dev.off()
-
-setEPS()
-postscript("Real_plot11.eps",width = 9, height = 6)
-par(mar = c(4, 5.5, 2, 2))
-qqplot(res_tn_boot_K2_g1, res_tn_boot_q, ylab=expression("Quantiles of "*hat(T)[n]*" from "*G[beta]),
-       xlab=expression("Quantiles of "*hat(T)[n]*" from "*F[gamma]*""),
-       lwd = 2, cex.axis = 1.6, cex.lab = 1.8)
-abline(0,1)
-dev.off()
-
-
-## Order selection statistics, ordered
-setEPS()
-postscript("MNRAS3.eps",width = 9, height = 6)
-par(mar = c(4, 5.5, 2, 2))
-qqplot(res_tn_boot_g1, res_tn_order_boot_q, ylab=expression("Quantiles of "*tilde(T)[n]*" from "*G[beta]),
-       xlab=expression("Quantiles of "*tilde(T)[n]*" from "*F[gamma]*" without K-2"),
-       lwd = 2, cex.axis = 1.6, cex.lab = 1.8)
-abline(0,1)
-dev.off()
-
-setEPS()
-postscript("Real_plot22.eps",width = 9, height = 6)
-par(mar = c(4, 5.5, 2, 2))
-qqplot(res_tn_order_boot_K2_g1, res_tn_order_boot_q, ylab=expression("Quantiles of "*tilde(T)[n]*" from "*G[beta]),
-       xlab=expression("Quantiles of "*tilde(T)[n]*" from "*F[gamma]),
-       lwd = 2, cex.axis = 1.6, cex.lab = 1.8)
-abline(0,1)
-dev.off()
-
-
 
 
 #################################### p-values #################################### 
@@ -397,10 +293,56 @@ for (j in 1:M1){
 res_tn_bs11 <- max(cumsum((vn_bs11)^2)/1:M1)
 res_tn_order_bs11 <- max(cumsum(sort((vn_bs11)^2,decreasing=TRUE))/1:M1)
 
-print((sum(res_tn_boot_q>=res_tn_bs11)+1)/(B+1))
-print((sum(res_tn_order_boot_q>=res_tn_order_bs11)+1)/(B+1))
+print((sum(res_tn_boot_f>=res_tn_bs11)+1)/(B+1))
+print((sum(res_tn_order_boot_f>=res_tn_order_bs11)+1)/(B+1))
 
 
-print((sum(res_tn_boot_K2_g1>=res_tn_bs11)+1)/(length(res_tn_boot_K2_g1)+1))
-print((sum(res_tn_order_boot_K2_g1>=res_tn_order_bs11)+1)/(length(res_tn_order_boot_K2_g1)+1))
+#################################### QQ-plots #################################### 
+### This generates the Figure 4 in Section 5. Warning: this will be slow
 
+sampler_c = function(n, cc){
+  tt = rlst(2*n,df=4,sigma=0.025)
+  res = (tt[tt>=L1+0.015-cc & tt<=U1-0.015-cc])[1:n]
+  return(rtruncnorm(n,a=cc-0.015,b=cc+0.015,cc,sigma) + res)
+}
+sampler_x1 = function(n){
+  indx = rmultinom(1,size=n,prob=c(1-sum(MLEg1),MLEg1))
+  return (c(runif(indx[1],L1,U1), sampler_c(indx[2],pos[1]), sampler_c(indx[3],pos[2]),sampler_c(indx[4],pos[3])))
+}
+
+set.seed(1)
+start_time2 <- Sys.time()
+res_tn_order_boot_K2_g1 <- res_tn_boot_K2_g1 <- numeric(B);vn_boot_K2_g1 <- matrix(NA,nrow=B,ncol=M1)
+for (k in 1:B){
+  xx_k2 = sampler_x1(n)
+  for (j in 1:M1){
+    vn_boot_K2_g1[k,j] <- sqrt(n)*mean(Kvj1[[j+1]](xx_k2))
+  }
+  res_tn_boot_K2_g1[k] <- max(cumsum((vn_boot_K2_g1[k,])^2)/1:M1)
+  res_tn_order_boot_K2_g1[k] <- max(cumsum(sort((vn_boot_K2_g1[k,])^2,decreasing=TRUE))/1:M1)
+  print(k)
+}
+end_time2 <- Sys.time()
+end_time2-start_time2
+
+
+#################################### QQplots #################################### 
+setEPS()
+postscript("Real_plot11.eps",width = 9, height = 6)
+par(mar = c(4, 5.5, 2, 2))
+qqplot(res_tn_boot_K2_g1, res_tn_boot_f, ylab=expression("Quantiles of "*hat(T)[n]*" from "*G[beta]),
+       xlab=expression("Quantiles of "*hat(T)[n]*" from "*F[gamma]*""),
+       lwd = 2, cex.axis = 1.6, cex.lab = 1.8)
+abline(0,1)
+dev.off()
+
+
+## Order selection statistics, ordered
+setEPS()
+postscript("Real_plot22.eps",width = 9, height = 6)
+par(mar = c(4, 5.5, 2, 2))
+qqplot(res_tn_order_boot_K2_g1, res_tn_order_boot_f, ylab=expression("Quantiles of "*tilde(T)[n]*" from "*G[beta]),
+       xlab=expression("Quantiles of "*tilde(T)[n]*" from "*F[gamma]),
+       lwd = 2, cex.axis = 1.6, cex.lab = 1.8)
+abline(0,1)
+dev.off()
